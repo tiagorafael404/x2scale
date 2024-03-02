dfba2634f4dbb85fac3c96affc69d57f11c04ad8
tiagorafael404 <161669844+tiagorafael404@users.noreply.github.com>
2024-03-02 16:36:11 +0000

    nao sei

---
+++ home/site.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>
+
+</title>
+
+<link rel="stylesheet" href="css.css">
+
+</head>
+<body>
+
+<div class="screen">
+
+<div class="navbar">
+<ul>
+
+<li><a href="#">Home</a></li>
+<li><a href="#">Projects</a></li>
+<li><a href="#">School</a></li>
+
+</ul>
+</div>
+
+<div class="content">
+
+<div class="part1">
+<div class="part1a">
+    <div class="dm">
+        <div class="dm1"><div class="pp"></div></div>
+        <div class="dm2">
+            <div class="dm-info">
+                <div class="dm-name"><a>@Obama</a></div><div class="dm-date"><a>03/02/2024 03:15 PM</a></div>
+            </div>
+            <div class="dm-msg"><div class="dm-text"><a>Este site não tem nada de interessante e a tua presença não contribui</a></div></div>
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="part2">
+
+<div class="part2a">
+
+    <div class="p2a"></div>
+
+    <div class="p2b">
+    <li><a href="#">sei la</a></li>
+    </div>
+
+</div>
+
+</div>
+</div>
+
+</div>
+
+
+
+
+</body>
+</html>
