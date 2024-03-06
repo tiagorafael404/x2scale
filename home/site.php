4517070603c37bf40a7d5fc321e52c408a946ea8
tiagorafael404 <161669844+tiagorafael404@users.noreply.github.com>
2024-03-06 05:30:15 +0000

    a

--- home/site.php
+++ home/site.php
@@ -3,10 +3,12 @@
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>
-
+X2Scale
 </title>
 
 <link rel="stylesheet" href="css.css">
+<link rel="icon" href="imagem.ico" type="image/x-icon">
+
 
 </head>
 <body>
