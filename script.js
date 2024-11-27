aaba40531c8094c54c890ff20ab5259acb3c95ec
tiagorafael404 <161669844+tiagorafael404@users.noreply.github.com>
2024-11-27 23:57:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-    const projectDivs = document.querySelectorAll(".projects > div"); // Seleciona apenas os filhos diretos de .projects
+    const mediaQuery = window.matchMedia("(min-width: 1251px)");
 
-    projectDivs.forEach(div => {
-        div.addEventListener("mouseenter", () => {
-            div.style.transition = "transform 0.3s"; // Animação suave
-            div.style.transform = "translateY(-50px)"; // Move para cima
-        });
+    if (mediaQuery.matches) { // Executa somente se a largura for >= 1200px
+        const projectDivs = document.querySelectorAll(".projects > div"); // Seleciona apenas os filhos diretos de .projects
 
-        div.addEventListener("mouseleave", () => {
-            div.style.transform = "translateY(0px)"; // Volta à posição original
+        projectDivs.forEach(div => {
+            div.addEventListener("mouseenter", () => {
+                div.style.transition = "transform 0.3s"; // Animação suave
+                div.style.transform = "translateY(-50px)"; // Move para cima
+            });
+
+            div.addEventListener("mouseleave", () => {
+                div.style.transform = "translateY(0px)"; // Volta à posição original
+            });
         });
-    });
+    }
 });
-
-
